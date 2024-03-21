09b6f9e970566a2ec2297bfa30b88ae972525af0
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-03-21 10:16:51 -0300

    exerc 21/03/2024

---
+++ Funcao01/src/fncao01/Funcao01.java
@@ -0,0 +1,13 @@
+package fncao01;
+
+public class Funcao01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Fatorial f = new Fatorial();
+        f.setvalor(5);
+        System.out.println(f.getFormula());  
+        System.out.println(f.getFatorial());
+	}
+
+}
